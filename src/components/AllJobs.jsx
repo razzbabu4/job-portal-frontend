507c3ef2631dd451f18d42578d6a56d39497c27a
Tareq <razzbabut@gmail.com>
2024-12-14 01:02:29 +0600

    added loader for fetching data

--- src/components/AllJobs.jsx
+++ src/components/AllJobs.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { axiosInstance } from "../api/axiosInstance";
 import JobCard from "./JobCard";
+import { Loader } from "./Loader";
 
 
 const AllJobs = () => {
     const [jobs, setJobs] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchJobs = async () => {
@@ -14,6 +16,8 @@ const AllJobs = () => {
                 setJobs(response.data.data);
             } catch (error) {
                 console.error("Error fetching jobs:", error);
+            } finally {
+                setIsLoading(false);
             }
         };
 
@@ -22,12 +26,18 @@ const AllJobs = () => {
 
     return (
         <div className="container mx-auto">
-            <h1 className="text-xl font-bold mb-4">All Jobs</h1>
-            <div className="grid gap-4 grid-cols-1 md:grid-cols-2">
-                {jobs.map((job) => (
-                    <JobCard key={job._id} job={job} />
-                ))}
-            </div>
+            <h1 className="text-2xl font-bold mb-4">All Jobs</h1>
+            {isLoading ? (
+                <Loader />
+            ) : (
+                <div className="grid gap-4 grid-cols-1 md:grid-cols-2">
+                    {jobs.length > 0 ? (
+                        jobs.map((job) => <JobCard key={job._id} job={job} />)
+                    ) : (
+                        <p className="text-gray-500">No job listings available.</p>
+                    )}
+                </div>
+            )}
         </div>
     );
 };
---
+++ src/components/Loader.jsx
@@ -0,0 +1,5 @@
+export const Loader = () => (
+    <div className="flex justify-center items-center mt-20">
+        <div className="border-t-4 border-blue-600 border-solid rounded-full w-10 h-10 animate-spin"></div>
+    </div>
+);
